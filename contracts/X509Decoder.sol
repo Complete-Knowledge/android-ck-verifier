48fbab37353cdd22e0d5fcff39bf5b6ffdeb7982
jaa2 <43010335+jaa2@users.noreply.github.com>
2022-12-03 06:29:46 -0500

    Complete full chain verification

--- contracts/X509Decoder.sol
+++ contracts/X509Decoder.sol
@@ -13,11 +13,22 @@ import "./DateTime.sol";
 // Debugging
 import "hardhat/console.sol";
 
+/**
+ * @dev A contract that attests to proofs of complete knowledge
+ */
+interface ICKVerifier {
+    /**
+     * @dev Returns true if the address has shown a proof of complete knowledge
+     * to this verifier.
+     */
+    function isCKVerified(address addr) external returns (bool);
+}
+
 /*
  * @dev Stores validated X.509 certificate chains in parent pointer trees.
  * @dev The root of each tree is a CA root certificate
  */
-contract X509Parser is Ownable {
+contract X509Parser is Ownable, ICKVerifier {
   using Asn1Decode for bytes;
   using ENSNamehash for bytes;
 
@@ -68,8 +79,14 @@ contract X509Parser is Ownable {
    * @param cert A DER-encoded X.509 certificate
    * @param certParams The parent certificate's DER-encoded public key
    */
+  
+  struct ParentParams {
+    bytes pubKey;
+    bool verified;
+  }
+  
   struct CertParams {
-    bytes parentPubKey;
+    ParentParams parent;
     address proverAddress;
   }
 
@@ -78,26 +95,155 @@ contract X509Parser is Ownable {
   mapping (bytes32 => bytes32[])   public  toCertIds; // ensNamehash(subjectAltName) => certId
   mapping (bytes32 => bytes32)     public  toCertId;  // sha256 fingerprint => certId
   DateTime dateTime;                                  // For dateTime conversion
+  
+  // Trusted by the contract owner
+  mapping (bytes32 => bool)        public  trustedKeys;
+  // Untrusted by the contract owner; revocation list
+  mapping (bytes32 => bool)        public  revokedKeys;
+  mapping (address => bool)        public  isCKVerified;
 
   event CertAdded(bytes32);
   event CertClaimed(bytes32, address);
   event AlgSet(bytes32, address);
+  
+  /**
+   * @dev Mark a key as trusted. Certificates signed by this key are considered
+   * trusted unless they have been revoked.
+   * @param pubKey List of public keys to trust
+   * @param trust If true, trust these keys. If false, do not trust
+   *    these keys by themselves.
+   */
+  function setTrustedKeys(bytes[] calldata pubKey, bool trust) public onlyOwner {
+    for (uint i = 0; i < pubKey.length; i++) {
+      trustedKeys[keccak256(pubKey[i])] = trust;
+    }
+  }
+  
+  /**
+   * @dev Mark a key as revoked. Certificates signed by this key are considered
+   * invalid.
+   * @param pubKey List of public keys to revoke
+   * @param revoke If true, revoke these keys. If false, allow them to be used
+   *    in a chain of trust.
+   */
+  function setRevokedKeys(bytes[] calldata pubKey, bool revoke) public onlyOwner {
+    for (uint i = 0; i < pubKey.length; i++) {
+      revokedKeys[keccak256(pubKey[i])] = revoke;
+    }
+  }
+  
+  function checkParentTrusted(ParentParams memory parent) public view {
+    if (!parent.verified) {
+      parent.verified = trustedKeys[keccak256(parent.pubKey)];
+      require(parent.verified, "Certificate chain not trusted");
+    }
+    require(!revokedKeys[keccak256(parent.pubKey)], "Parent public key revoked");
+  }
+  
+  /**
+   * @dev Verify a certificate chain ending in a lightweight CK proof
+   * @param certChain Certificate chain to verify, starting with the attestation cert
+   * @param parentPubKey The public key of the trusted parent certificate
+   * @param proverAddress Address the proof is for
+   */
+  function verifyCertChain(bytes[] calldata certChain, bytes calldata parentPubKey, address proverAddress) public {
+    ParentParams memory parent;
+    parent.pubKey = parentPubKey;
+    parent.verified = false;
+    for (int i = int256(certChain.length) - 1; i >= 1; i--) {
+      parent.pubKey = addCertToChain(certChain[uint256(i)], parent);
+      parent.verified = true;
+    }
+    CertParams memory certParams;
+    certParams.parent = parent;
+    certParams.proverAddress = proverAddress;
+    addKeyCert(certChain[0], certParams);
+    isCKVerified[proverAddress] = true;
+  }
+  
+  /**
+   * @dev Verify a certificate is signed with a verified parent public key
+   * @param cert Certificate to verify
+   * @param parent The public key of the parent certificate and if the parent key
+   * has been linked to a root of trust
+   * @return Public key of this certificate
+   */
+  function addCertToChain(bytes memory cert, ParentParams memory parent) public view returns (bytes memory) {
+    checkParentTrusted(parent);
+    Certificate memory certificate;
+    // Certificate root
+    uint node1 = cert.root();
+    // tbsCertificate (TBSCertificate)
+    node1 = cert.firstChildOf(node1);
+    // |-- Version
+    uint node2 = cert.firstChildOf(node1);
+    require(cert.uintAt(cert.firstChildOf(node2)) + 1 == 3, "Certificate must be X.509 Version 3");
+    node2 = cert.nextSiblingOf(node2);
+    // signature node (AlgorithmIdentifier)
+    node2 = cert.nextSiblingOf(node2);
+    node2 = cert.firstChildOf(node2);
+    // signatureValue node (BIT STRING)
+    uint node3 = cert.nextSiblingOf(node1);
+    node3 = cert.nextSiblingOf(node3);
+    
+    Algorithm algAddress = algs[cert.bytes32At(node2)];
+    require(address(algAddress) != address(0x0), "Algorithm unknown");
+    
+    // Parse signature
+    node2 = cert.rootOfBitStringAt(node3);
+    node2 = cert.firstChildOf(node2);
+    require(algAddress.verify(bytes.concat(bytes4(0x00000000), parent.pubKey),
+        cert.allBytesAt(node1),
+        bytes.concat(cert.uintBytesAt(node2), cert.uintBytesAt(cert.nextSiblingOf(node2)))),
+        "Intermediate signature doesn't match");
+    
+    node1 = cert.firstChildOf(node1);
+    node1 = cert.nextSiblingOf(node1);
+    node1 = cert.nextSiblingOf(node1);
+    node1 = cert.nextSiblingOf(node1);
+    node1 = cert.nextSiblingOf(node1);
+    
+    // Now at validity (Validity)
+    node2 = cert.firstChildOf(node1);
+    // Check notBefore
+    certificate.validNotBefore = uint40(toTimestamp(cert.bytesAt(node2)));
+    require(certificate.validNotBefore <= block.timestamp, "Now is before validNotBefore");
+    node2 = cert.nextSiblingOf(node2);
+    // Check notAfter
+    certificate.validNotAfter = uint40(toTimestamp(cert.bytesAt(node2)));
+    require(block.timestamp <= certificate.validNotAfter, "Now is after validNotAfter");
+
+    // subject (Name)
+    node1 = cert.nextSiblingOf(node1);
+    // subjectPublicKeyInfo (SubjectPublicKeyInfo)
+    node1 = cert.nextSiblingOf(node1);
+    
+    node2 = cert.firstChildOf(node1);
+    node2 = cert.nextSiblingOf(node2);
+    
+    // Get this certificate's public key
+    bytes memory certPublicKey = cert.bitstringAt(node2);
+    require(certPublicKey.length == 65, "Public key length incorrect");
+    // Cut off the 0x04 (indicating it is an uncompressed EC public key)
+    return BytesUtils.substring(certPublicKey, 1, 64);
+  }
 
   /**
    * @dev Add a X.509 certificate to an existing tree/chain
    * @param cert A DER-encoded X.509 certificate
    * @param certParams A description of the certificate and intended address to verify
    */
-  function addCert(bytes memory cert, CertParams memory certParams)
-  public
+  function addKeyCert(bytes memory cert, CertParams memory certParams)
+  public view
   {
+    checkParentTrusted(certParams.parent);
     Certificate memory certificate;
     uint node1;
     uint node2;
     uint node3;
     uint node4;
 
-    certificate.parentId = keccak256(certParams.parentPubKey);
+    certificate.parentId = keccak256(certParams.parent.pubKey);
     certificate.timestamp = uint40(block.timestamp);
     
     // Follow along with parsing the X.509 cert at
@@ -153,11 +299,11 @@ contract X509Parser is Ownable {
     console.log("sig bytes");
     console.logBytes(bytes.concat(cert.uintBytesAt(node2), cert.uintBytesAt(cert.nextSiblingOf(node2))));
     console.log("pub key bytes");
-    console.logBytes(certParams.parentPubKey);
+    console.logBytes(certParams.parent.pubKey);
     
     // verify(pubkey, data, signature)
     {
-    require(algAddress.verify(certParams.parentPubKey, cert.allBytesAt(node1),
+    require(algAddress.verify(bytes.concat(bytes4(0x00000000), certParams.parent.pubKey), cert.allBytesAt(node1),
         bytes.concat(cert.uintBytesAt(node2), cert.uintBytesAt(cert.nextSiblingOf(node2)))),
     "Signature doesn't match");
     }
@@ -380,8 +526,6 @@ contract X509Parser is Ownable {
       }
     }
     return;
-
-    require(certs[certificate.parentId].cA, "Invalid parent cert");
 
     // TODO: Handle 4.2.1.9. of RFC 5280 (cert constraints)
   }
